d1bf80671f06b685cee263d8d9c002b2a79c0f7e
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-06-30 10:37:08 -0700

    'Integrate'

--- post-routes.js
+++ post-routes.js
@@ -1,30 +1,97 @@
 const express = require('express');
 const router = express.Router();
 
-// const {BlogPosts} = require('./models');
+const { BlogPost } = require('./models');
 
 //Begin get request
 router.get('/', (req, res) => {
     // res.json(BlogPosts.get());
-    res.send('GET /posts')
+    BlogPost
+    .find()
+    .then(posts => {
+        res.json(posts.map(post => post.serialize()));
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'done messed up.'});
+    });
+    // res.send('GET /posts')
 });
 
 router.get('/:id', (req,res) => {
-    res.send('GET /posts/:id')
+    BlogPost
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'done messed up.'});
+    });
+    // res.send('GET /posts/:id')
 });
 //Begin post request with create function
 router.post('/', (req, res) => {
-    res.send('POST /posts')
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i < requiredFields.length; i++){
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    // res.send('POST /posts')
+    BlogPost
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+        .then(blogPost => res.status(201).json(blogPost.serialize()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went wrong' })
+        });
+
 });
 
 //Begin put request with update function 
 router.put('/:id', (req, res) => {
-    res.send('PUT /posts/:id')
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
+    }
+
+    const updated = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+
+    BlogPost
+        .findByIdandRemove(req.params.id)
+        .then(() => {
+            console.log(`Deleted blog post with id \`${req.params.id}\``);
+            res.status(204).end();
+        });
+    // res.send('PUT /posts/:id')
 });
 
 //Begin delete request
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /posts/:id')
+    BlogPost
+        .findByIdandRemove(req.params.id)
+        .then(() => {
+            console.log(`Deleted blog post with id \`${req.params.id}\``);
+            res.status(204).end();
+        });
+    // res.send('DELETE /posts/:id')
+});
+
+router.use('*', function (req, res) {
+    res.status(404).json({ message: 'Not Found'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { DATABASE_URL, PORT} = require('./config');
 
 const app = express();
 
@@ -11,33 +15,40 @@ app.use('/posts', require('./post-routes'));
 
 let server;
 
-function runServer() {
-    const port = process.env.PORT || 8080;
+function runServer(databaseUrl, port = PORT) {
     return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err);
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
         });
     });
 }
 
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if (err) {
-                reject(err);
-                return;
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
             }
             resolve();
+            });
         });
     });
 }
 
 if (require.main === module) {
-    runServer().catch(err => console.error(err));
+    runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = {app, runServer, closeServer};
